8627db4a088d71253e999addfacc1932a493bf3f
lizhi <15501251520@163.com>
2022-06-14 19:06:56 +0800

    feat: vue3-vite project add env

--- vue3-ts-vite-project/vite.config.ts
+++ vue3-ts-vite-project/vite.config.ts
@@ -3,8 +3,24 @@ import vue from '@vitejs/plugin-vue'
 
 const path = require('path')
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // 代理
+  // server: {
+  //   host: '0.0.0.0',
+  //   port: 3000,
+  //   open: true,
+  //   https: false,
+  //   proxy: {},
+  // },
+  //
+  // build: {
+  //   terserOptions: {
+  //     compress: {
+  //       drop_console: true,
+  //       drop_debugger: true,
+  //     },
+  //   },
+  // },
   plugins: [vue()],
   resolve: {
     alias: {
