f0fc01a0e08b82034d0c2f8720548f23f44f3509
lizhi <15501251520@163.com>
2022-06-15 17:27:34 +0800

    feat: add compression

--- vue3-ts-vite-project/vite.config.ts
+++ vue3-ts-vite-project/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression'
 
 const path = require('path')
 
@@ -21,7 +22,17 @@ export default defineConfig({
   //     },
   //   },
   // },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // gzip压缩 生产环境生成 .gz 文件
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
